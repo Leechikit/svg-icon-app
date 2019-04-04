1ccefb9eba546a196992960f9d1de2aacffc8582
leechikit <leechikit0823@163.com>
2019-04-04 15:51:05 +0800

    update

---
+++ src/components/SvgSpritesIcon.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <icon icon-name="facebook"></icon>
+    <icon
+      icon-name="twitter"
+      width="48"
+      height="48"
+    ></icon>
+    <icon
+      icon-name="google"
+      width="64"
+      height="64"
+      icon-color="green"
+    ></icon>
+  </div>
+</template>
--- src/main.js
+++ src/main.js
@@ -1,5 +1,8 @@
 import Vue from 'vue'
 import App from './App.vue'
+import iconComponent from './components/svgSprites/Icon'
+
+Vue.component('icon', iconComponent)
 
 Vue.config.productionTip = false
 

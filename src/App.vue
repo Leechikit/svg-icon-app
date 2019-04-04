f4e14b1c58e66988659f71f3b328f7ceb21ec31b
leechikit <leechikit0823@163.com>
2019-04-04 15:38:17 +0800

    update

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,18 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <VueOfficialSvgIcons></VueOfficialSvgIcons>
   </div>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import VueOfficialSvgIcons from '@/components/VueOfficialSvgIcons.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    HelloWorld,
+    VueOfficialSvgIcons
   }
 }
 </script>
---
+++ src/components/VueOfficialSvgIcons.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="icons">
+    <IconBox
+      width="16"
+      height="16"
+      icon-color="#f36"
+      icon-name="facebook"
+    >
+      <Facebook />
+    </IconBox>
+    <IconBox
+      icon-color="#f0987a"
+      icon-name="twitter"
+    >
+      <Twitter />
+    </IconBox>
+    <IconBox
+      width="48"
+      height="48"
+      icon-name="google plus"
+    >
+      <GooglePlus />
+    </IconBox>
+  </div>
+</template> 
+<script> 
+import IconBox from './icons/IconBox'
+import Facebook from './icons/Facebook'
+import Twitter from './icons/Twitter'
+import GooglePlus from './icons/GooglePlus'
+export default {
+  name: 'VueOfficialSvgIcons',
+  components: {
+    IconBox,
+    Facebook,
+    Twitter,
+    GooglePlus
+  }
+}
+</script>
